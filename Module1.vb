14321600e8db50da834508c1ccaf7e7f2b5c937c
AamnaSaleem <93542393+AamnaSaleem@users.noreply.github.com>
2021-11-16 21:11:32 +0500

    Add files via upload

---
+++ Module1.vb
@@ -0,0 +1,20 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim NotPos, BadPos As Integer
+        Dim FP, SP, str1 As String
+
+        Console.Write("Enter line to be processed: ")
+        str1 = Console.ReadLine
+
+        NotPos = InStr(str1, "not")
+        BadPos = InStr(str1, "bad")
+
+        FP = Left(str1, NotPos - 1)
+        SP = Right(str1, (Len(str1) - BadPos - 4))
+
+        Console.WriteLine(FP & " good " & SP)
+        Console.ReadKey()
+    End Sub
+
+End Module
